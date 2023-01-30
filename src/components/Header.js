130bbb681688aee3177e045f7fbacdbf0e7d6da4
Iamohwoevwomeshack <ohwoevwomeshack@gmail.com>
2023-01-30 05:07:47 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { MdHighlight } from "react-icons/md"
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <MdHighlight/>
+        Keeper App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
